2e15fc4b10e86a210f3567e8360e545b93e3ea4d
Rakesh-Kumar347 <Rakesh.Kumar@iseyon.com>
2025-03-26 14:47:20 +0530

    fixed issue

--- lobo/backend/utils/__init__.py
+++ lobo/backend/utils/__init__.py
@@ -1,14 +1,14 @@
 # utils/__init__.py
 from .database import execute_query, insert_data, update_data, delete_data
 from .jwt_handler import generate_token, generate_refresh_token
-from .vector_db import store_vectors, load_vector_store
+from .vector_db import store_vectors, search_vectors
 from .logger import setup_logger
-from .api_response import success_response,error_response
+from .api_response import success_response, error_response
 from .cache import initialize_cache
 
 __all__ = [
     "execute_query", "insert_data", "update_data", "delete_data",
     "generate_token", "generate_refresh_token",
-    "store_vectors", "load_vector_store",
-    "setup_logger", "success_response" , "error_response"
+    "store_vectors", "search_vectors",
+    "setup_logger", "success_response", "error_response"
 ]
